4a9a3274827bd28b73024305dc890eab8b38dd7d
LEE-DHARAN <muralidhawan@gmail.com>
2025-03-12 18:23:30 +0530

    12.3.25

---
+++ array_and_hashmap/grp_anagram.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+
+# fruits = ["mango","banana","pineapple"]
+# fruits.append("orange")
+# veggies = ["brinjal","potato","drumsticks"]
+# groceries = [fruits,veggies]
+# for fruit in fruits:
+#     print(fruit)
+# for gro in groceries:
+#     for g in gro:
+#         print(g)    
+
+
+hash_map = defaultdict(list) #emptylist
+hash_map["fruits"].append("mango")
+hash_map["fruits"].append("orange")       
+print(hash_map["fruits"])
