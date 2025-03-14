9aeda2121700bbc679ebb830886c0b534f7b6ef7
LEE-DHARAN <muralidhawan@gmail.com>
2025-03-14 17:21:45 +0530

    14.3.25

--- array_and_hashmap/grp_anagram.py
+++ array_and_hashmap/grp_anagram.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import List
 
 
 # fruits = ["mango","banana","pineapple"]
@@ -11,8 +12,18 @@ from collections import defaultdict
 #     for g in gro:
 #         print(g)    
 
-fruits = ["mango","banana","pineapple"]
-hash_map = defaultdict(list) #emptylist
-hash_map["fruits"].append("mango")
-hash_map["fruits"].append("orange")       
-print(hash_map["fruits"])
+# fruits = ["mango","banana","pineapple"]
+# hash_map = defaultdict(list) #emptylist
+# hash_map["fruits"].append("mango")
+# hash_map["fruits"].append("orange")       
+# print(hash_map["fruits"])
+
+
+def grp_anagram(self,wrds:List[str])->List[List[str]]:
+    hash_map = defaultdict(list)
+    for wrd in wrds:
+        count = [0] * 26
+        for char in wrd:
+            count[ord(char)-ord('a')] += 1
+        hash_map[tuple(count)].append(wrd)
+    return list(hash_map.values())
